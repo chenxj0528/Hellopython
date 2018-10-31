6a659b6ca618c815c5738c56a3727e19728bc1e8
python <chenxj0528@gmail.com>
2018-10-31 14:27:01 +0800

    ok

--- vehice.py
+++ vehice.py
@@ -2,10 +2,9 @@ def jiecheng(x):
     if x == 1 or x == 2:
         return 1
     else:
-        return jiecheng(x-1) + jiecheng(x-2)
+        return jiecheng(x - 1) + jiecheng(x - 2)
+
 
 a = jiecheng(40)
 
 print(a)
-
-
